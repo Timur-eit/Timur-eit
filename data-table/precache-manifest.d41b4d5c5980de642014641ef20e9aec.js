3eb6b008c2fb4bf0fc509c3e394e35d028b7847c
Evgeny <evtsyga@gmail.com>
2023-05-28 13:10:39 +0300

    Revert "delete all"
    This reverts commit f4481be2695561060d89a4a4e0d56386019e90af.

---
+++ data-table/precache-manifest.d41b4d5c5980de642014641ef20e9aec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ac31e7c22f4c112833a2ff89030932",
+    "url": "/data-table/index.html"
+  },
+  {
+    "revision": "72a502b509020f4766b3",
+    "url": "/data-table/static/css/main.fb8c166e.chunk.css"
+  },
+  {
+    "revision": "047b0036efe34ca39df0",
+    "url": "/data-table/static/js/2.3c256266.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/data-table/static/js/2.3c256266.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a502b509020f4766b3",
+    "url": "/data-table/static/js/main.fb871d39.chunk.js"
+  },
+  {
+    "revision": "cce6d5e32b50c6690363",
+    "url": "/data-table/static/js/runtime-main.68cde7a9.js"
+  },
+  {
+    "revision": "28735e20683547a4024d8c46a4ab95a1",
+    "url": "/data-table/static/media/cross-svgrepo-com.28735e20.svg"
+  },
+  {
+    "revision": "10bff67014e7a3ab53238d83a27b8539",
+    "url": "/data-table/static/media/edit-text-svgrepo-com.10bff670.svg"
+  },
+  {
+    "revision": "8950c04a2437e071d47f9227f55f9b41",
+    "url": "/data-table/static/media/search-svgrepo-com.8950c04a.svg"
+  }
+]);
